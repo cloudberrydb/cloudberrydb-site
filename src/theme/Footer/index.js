6764623e8c245e166b95ccdbfa7199287c7438d1
Dianjin Wang <wangdianjin@gmail.com>
2023-06-06 11:10:37 +0800

    update footer

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -3,16 +3,13 @@ import Footer from "@theme-original/Footer";
 import Docsly from "@docsly/react";
 import "@docsly/react/styles.css";
 import { useLocation } from "@docusaurus/router";
- 
+
 export default function FooterWrapper(props) {
   const { pathname } = useLocation();
-  if (pathname.startsWith("/docs")) {
   return (
     <>
       <Footer {...props} />
-      <Docsly publicId="private_lYJtMkHT9JmRH0vMOHvZU5M1VaQb1v1lNUckAiA9o1KGXSxgrY3kJMM5DG8LcWLx" pathname={pathname} />
+      {pathname.startsWith("/docs") && <Docsly publicId="private_lYJtMkHT9JmRH0vMOHvZU5M1VaQb1v1lNUckAiA9o1KGXSxgrY3kJMM5DG8LcWLx" pathname={pathname} />}
     </>
   );
-}
-  return null;
 }
