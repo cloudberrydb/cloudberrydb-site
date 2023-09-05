b6a043c021a2de4c24d86d469c602d95e188ac07
Dianjin Wang <wangdianjin@gmail.com>
2023-09-05 18:16:19 +0800

    Add Google Analytics to the site

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -65,6 +65,10 @@ const config = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        gtag: {
+          trackingID: 'G-5RF5B25JHD',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
