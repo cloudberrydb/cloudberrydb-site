631b2b4d54c8bb973ead03480ff28d8f18655d3a
TomShawn <41534398+TomShawn@users.noreply.github.com>
2024-04-02 15:48:24 +0800

    doc: add v1.5.1 release notes (#106)

--- sidebars.ts
+++ sidebars.ts
@@ -126,7 +126,7 @@ const sidebars: SidebarsConfig = {
         type: "doc",
         id: "releases/index",
       },
-      items: ["releases/release-1.5.0"],
+      items: ['releases/release-1.5.0','releases/release-1.5.1'],
     },
   ]
 }
